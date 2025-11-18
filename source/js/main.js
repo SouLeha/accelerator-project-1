47abe15f3161ef2021447764944f146b262d4087
SouLeha <strong0904@mail.ru>
2025-11-19 02:31:26 +0300

    Добавляет хедер полностью разметку и 3 вида вьюпорта, добавляет разметку хиро блок подключает слайдер, добавляет миксины, медиа запросы, добавляет блоки scss

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,29 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+// Базовая инициализация Swiper
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// Ждем загрузки DOM
+document.addEventListener('DOMContentLoaded', () => {
+  // Инициализация слайдера hero, если он есть на странице
+  const heroSlider = document.querySelector('.hero__slider');
+
+  if (heroSlider) {
+    new Swiper(heroSlider, {
+      modules: [Navigation, Pagination, Autoplay],
+      loop: true,
+      pagination: {
+        el: '.hero__pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 5000,
+      },
+    });
+  }
+});
